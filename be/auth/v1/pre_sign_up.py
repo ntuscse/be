31108c3c838427602987a9f49320948ba64c5328
ykcyberarts <101256521+ykcyberarts@users.noreply.github.com>
2022-05-12 22:10:23 +0800

    Throw Exception if invalid domain name

--- be/auth/v1/pre_sign_up.py
+++ be/auth/v1/pre_sign_up.py
@@ -1,12 +1,10 @@
 def lambda_handler(event, context):
-    # It sets the user pool autoConfirmUser flag after validating the email domain
-    event['response']['autoConfirmUser'] = False
-
+    print(f'DEBUG: Lambda Event {event}')
     # Split the email address so we can compare domains
     email: str = event['request']['userAttributes']['email']
 
     # Check if user is from a valid domain
     if email.endswith('ntu.edu.sg'):
-        event['response']['autoConfirmUser'] = True
+        return event
 
-    return event
+    raise Exception('Invalid domain name')
