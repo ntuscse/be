3762878b85426062dd697a0a9b6adc19b9aeb4f2
ykcyberarts <101256521+ykcyberarts@users.noreply.github.com>
2022-05-12 22:34:29 +0800

    Fix tests

--- be/auth/v1/pre_sign_up_test.py
+++ be/auth/v1/pre_sign_up_test.py
@@ -1,4 +1,5 @@
 from be.auth.v1.pre_sign_up import lambda_handler
+import pytest
 
 
 def test_pre_sign_up_valid_ntu_email():
@@ -14,7 +15,7 @@ def test_pre_sign_up_valid_ntu_email():
     }
 
     event_response = lambda_handler(event_request, None)
-    assert event_response['response']['autoConfirmUser'] == True
+    assert event_response == event_request
 
 def test_pre_sign_up_invalid_ntu_email():
     event_request = {
@@ -28,5 +29,5 @@ def test_pre_sign_up_invalid_ntu_email():
         }
     }
 
-    event_response = lambda_handler(event_request, None)
-    assert event_response['response']['autoConfirmUser'] == False
+    with pytest.raises(Exception, match='Invalid domain name'):
+        lambda_handler(event_request, None)
