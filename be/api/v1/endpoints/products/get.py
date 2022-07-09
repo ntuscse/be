275893689e4fefa85e640574a62a1b650b23976b
Dyllon <dyllon.g@outlook.com>
2022-07-10 03:01:24 +0800

    Add dummy data to get_products endpoint

--- be/api/v1/endpoints/products/get.py
+++ be/api/v1/endpoints/products/get.py
@@ -1,39 +1,22 @@
 import os
 from fastapi import APIRouter, HTTPException
 from be.api.v1.templates.non_auth_route import create_non_auth_router
-from be.api.v1.models.product import Product
+from be.api.v1.endpoints.products.data import products
 
 router = APIRouter(prefix="/products")
-
-products = [
-    Product(id="1",
-            name="Product 1",
-            price=12.50,
-            images=[''],
-            sizes=['xs', 's', 'm', 'l'],
-            productCategory='',
-            ),
-    Product(id="2",
-            name="Product 2",
-            price=17.50,
-            images=[''],
-            sizes=['xs', 's', 'm', 'l', 'xl'],
-            productCategory='',
-            ),
-]
 
 
 @router.get("", tags=["merchandise"])
 # gets all products
 async def get_products():
-    table_name = os.environ.get("PRODUCTS_TABLE_NAME")
+    # table_name = os.environ.get("PRODUCTS_TABLE_NAME")
     return {'products': products}
 
 
 @router.get("/{item_id}")
 # gets a single product
 async def get_product(item_id):
-    for index, item in enumerate(products):
+    for _index, item in enumerate(products):
         if item.id == item_id:
             return item
     raise HTTPException(status_code=404, detail="Product not found")
