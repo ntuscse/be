1c6ddb9a4e11f4f68728b11b73a51446e7d20351
Dyllon <dyllon.g@outlook.com>
2022-07-16 13:24:30 +0800

    fix products get endpoint typing

--- be/api/v1/endpoints/products/get.py
+++ be/api/v1/endpoints/products/get.py
@@ -1,21 +1,25 @@
 # import os
 from fastapi import APIRouter, HTTPException
+from pydantic import BaseModel
 from be.api.v1.templates.non_auth_route import create_non_auth_router
 from be.api.v1.endpoints.products.data import products
+from be.api.v1.models.product import Product
 
-router = APIRouter(prefix="/products")
+router = APIRouter(prefix="/products", tags=["merchandise"])
 
+class GetProductsResponseModel(BaseModel):
+    products: list[Product]
 
-@router.get("", tags=["merchandise"])
+@router.get("", response_model=GetProductsResponseModel)
 # gets all products
 async def get_products():
     # table_name = os.environ.get("PRODUCTS_TABLE_NAME")
     return {"products": products}
 
 
-@router.get("/{item_id}")
+@router.get("/{item_id}", response_model=Product)
 # gets a single product
-async def get_product(item_id):
+async def get_product(item_id: int):
     for _index, item in enumerate(products):
         if item.id == item_id:
             return item
