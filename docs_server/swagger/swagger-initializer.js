ee1c8b14f6c25b194b88497024b7a3b1106b5b00
Dyllon <dyllon.g@outlook.com>
2022-07-16 14:37:37 +0800

    fix vercel deployment domain

--- docs_server/swagger/swagger-initializer.js
+++ docs_server/swagger/swagger-initializer.js
@@ -3,7 +3,7 @@ window.onload = function() {
 
   // the following lines will be replaced by docker/configurator, when it runs in a docker-container
   window.ui = SwaggerUIBundle({
-    url: "https://api.docs.dev.ntuscse.com/openapi.json",
+    url: window.location.origin + "/openapi.json",
     dom_id: '#swagger-ui',
     deepLinking: true,
     presets: [
