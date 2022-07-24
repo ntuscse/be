ba702217abe9193d40c5df44010164b512eccb7d
Dyllon <dyllon.g@outlook.com>
2022-07-24 16:58:40 +0800

    Fix cors

--- be/api/v1/templates/non_auth_route.py
+++ be/api/v1/templates/non_auth_route.py
@@ -1,9 +1,26 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from mangum import Mangum
+
+origins = [
+    "https://api.ntuscse.com",
+    "https://api.dev.ntuscse.com",
+    "http://localhost",
+    "http://localhost:4000",
+]
 
 
 def create_non_auth_router(router):
     app = FastAPI()
+
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=origins,
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
+
     app.include_router(router)
 
     mangum_instance = Mangum(app)
