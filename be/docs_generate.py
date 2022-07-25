c8429d02a1319a5061003100422645e17c8ae3bf
Dyllon <dyllon.g@outlook.com>
2022-07-26 02:39:07 +0800

    fix docs - add vercel branding

--- be/docs_generate.py
+++ be/docs_generate.py
@@ -1,12 +1,7 @@
 import os
 from os import makedirs
 from os.path import join, dirname
-from fastapi.openapi.utils import get_openapi
 import json
-from dotenv import load_dotenv
-
-load_dotenv(join(dirname(__file__), '..', '.test.env'))
-
 from be.docs import app
 
 filePath = join(dirname(__file__), '..', 'out', 'openapi.json')
@@ -15,18 +10,4 @@ makedirs(dirname(filePath), exist_ok=True)
 base_api_server_url: str = os.getenv('BASE_API_SERVER_URL')
 
 with open(filePath, 'w') as f:
-    openapi_schema = get_openapi(
-        title=app.title,
-        version=app.version,
-        openapi_version=app.openapi_version,
-        description=app.description,
-        routes=app.routes,
-        servers=[{"url": base_api_server_url}]
-        # openapi_prefix=app.openapi_prefix,
-    )
-
-    openapi_schema["info"]["x-logo"] = {
-        "url": "https://www.datocms-assets.com/31049/1618983297-powered-by-vercel.svg"
-    }
-
-    json.dump(openapi_schema, f)
+    json.dump(app.openapi(), f)
