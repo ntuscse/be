9aabe68c4eefd06c37eed195d4f5939e024ac0ee
Dyllon <dyllon.g@outlook.com>
2022-12-05 18:40:05 +0800

    update cors origins

--- be/api/v1/templates/non_auth_route.py
+++ be/api/v1/templates/non_auth_route.py
@@ -7,13 +7,12 @@ is_dev = os.environ.get('SERVERLESS_STAGE')
 
 origins = [
     "http://localhost:3000",
-    "https://dev.ntuscse.com",
+    "https://dev.merch.ntuscse.com",
     "https://api.docs.dev.ntuscse.com"
 ] if is_dev else [
-    "https://ntuscse.com",
+    "https://merch.ntuscse.com",
     "https://api.docs.ntuscse.com"
 ]
-
 
 def create_non_auth_router(router):
     app = FastAPI()
