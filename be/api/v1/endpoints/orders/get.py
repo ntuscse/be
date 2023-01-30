f9a17720966ddfca84abb8ba6610db326d4b67d1
Chan Wen Xu <lutherchanpublic@gmail.com>
2023-01-30 23:10:26 +0800

    fix: Add endpoint to fetch orders
    [SCSE-177]

---
+++ be/api/v1/endpoints/orders/get.py
@@ -0,0 +1,17 @@
+from fastapi import APIRouter, HTTPException
+from be.api.v1.templates.non_auth_route import create_non_auth_router
+from be.api.v1.models.orders import Order
+from utils.dal.order import dal_read_order
+
+router = APIRouter(prefix="/orders", tags=["merchandise"])
+
+@router.get("/{order_id}", response_model=Order)
+# gets a single order
+async def get_order(order_id: str):
+    try:
+        return dal_read_order(order_id)
+    except Exception:
+        raise HTTPException(status_code=404, detail="Order not found")
+
+
+handler = create_non_auth_router(router)
